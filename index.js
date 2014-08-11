8a27d17c98b6507e94276aeaebecebd40fd837ec
Marc Bachmann <marc.brookman@gmail.com>
2014-08-11 22:17:25 +0200

    remove addJob. It would result in conflicts because of shiftStash

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-module.exports = require('./lib/migrate');
+var FileStorageMigration = require('./lib/file-storage-migration');
+exports = module.exports = FileStorageMigration;
+exports.version = '0.1.3';
+exports.Migration = require('./lib/migration');
+exports.MigrationManager = require('./lib/migration-manager');
+exports.FileStorageMigration = FileStorageMigration;
